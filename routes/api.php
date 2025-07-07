0ab239fe95b4c2769d2855c0f2f834f511d6130a
unknown <cyrelljaneradoc14@gmail.com>
2025-07-07 15:03:32 +0800

    added Loyalty program api

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,13 @@
 
 use App\Http\Controllers\Api\CompanyController;
 use App\Http\Controllers\ItemController;
+use App\Http\Controllers\LoyaltyProgramController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('company', CompanyController::class);
 Route::apiResource('items', ItemController::class);
+Route::apiResource('loyalty-program', LoyaltyProgramController::class);
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
