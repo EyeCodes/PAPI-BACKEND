10bdd68b3073f8043a9bd355761f7363a9dde4a0
lewdiedoeswubswubs <lanceedansantos@gmail.com>
2025-07-29 14:24:19 +0800

    All changes here are for the AI Backend for papi, we can integrate it into the main branch using the pre made @app/Services/FinancialAdvisorService
    I used PRISMPHP to implement tooling and structured outputs for the AI Agent, for better understanding of PrismPHP Library, check output
    prismphp.com for its documentation
    
    We will only integrate the papi ai chatbot into the main branch using the Financial advisor service, we can migrate the purchases table to the main branch
    as it can record each purchase and filter out if they are liabilities, or assets.
    
    If you have any questions feel free to approach or contact me. Since I'll be the one integrating it into the main branch!

---
+++ backend/app/Http/Resources/RewardsResource.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RewardsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+            //return parent::toArray($request);
+
+        return[
+            'id' => $this->id,
+            'loyalty_program_rule_id' => $this->loyalty_program_rule_id,
+            'reward_name' => $this->reward_name,
+            'description' => $this->description,
+            'reward_type' => $this->reward_type,
+            'point_cost' => $this->point_cost,
+            'discount_value' => $this->discount_value,
+            'discount_percentage' => $this->discount_percentage,
+
+            'item_id' => $this->item_id,
+            'voucher_code' => $this->voucher_code,
+            'is_active' => (bool) $this->is_active,
+            'max_redemption_rate' => $this->max_redemption_rate,
+            'expiration_days' => $this->expiration_days,
+            'users' => UserResource::collection($this->whenLoaded('users')),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at, 
+        ];
+    }
+}
+
+
